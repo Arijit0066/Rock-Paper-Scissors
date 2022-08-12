966fd03914b7062893d7bafe8e50f65f9eda29c6
Arijit0066 <108822902+Arijit0066@users.noreply.github.com>
2022-08-12 11:30:51 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,44 @@
+import random
+
+
+def game(comp, player):
+    if comp == player:
+        return None
+    elif comp == 'r':
+        if player == 'p':
+            return True
+        elif player == 's':
+            return False
+    elif comp == 'p':
+        if player == 's':
+            return True
+        elif player == 'r':
+            return False
+    elif comp == 's':
+        if player == 'r':
+            return True
+        elif player == 'p':
+            return False
+    
+
+
+
+print("Rock(r)  Paper(p)  Sizer(s)")
+rando = random.randint(1,3)
+if rando == 1:
+    comp = 'r'
+elif rando == 2:
+    comp = 'p'
+elif rando == 3:
+    comp = 's'
+
+print("Computer Already Choosed")
+player = input("Your Turn: ")
+final = game(comp, player)
+
+if final == None:
+    print("It's a Tie!!")
+elif final == True:
+    print("You Win!!")
+else:
+    print("You Lose!!")
